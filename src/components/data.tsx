b31773b487c059c7d4aec667268bbe30a0d322c1
l3alisidahmed <infouhbc001@gmail.com>
2025-01-30 17:34:26 +0100

    craete an array of fake data and export it

---
+++ src/components/data.tsx
@@ -0,0 +1,114 @@
+import Select from "@/components/ui/Select";
+
+export const data = [
+    {
+      name: "hamza 1234",
+      email: "hamza1234@gmail.com",
+      phone: "0555555555",
+      startDate: "2021-09-20",
+      store: {
+        id: "1234",
+        name: "store 1",
+      },
+      status: "active",
+      speciality: <Select 
+                    title="speciality" 
+                    isDisabled={true}
+                    width="w-[130px]"
+                    bgColor="bg-transparent" 
+                    borderWidth="border" 
+                    borderStyle="border-solid" 
+                    borderColor="border-black" 
+                    options={["speciality 1", "speciality 2", "speciality 3"]} 
+                />,
+      wilaya: "wilaya 1",
+    },
+    {
+      name: "hamza 1234",
+      email: "hamza1234@gmail.com",
+      phone: "0555555555",
+      startDate: "2021-09-20",
+      store: {
+        id: "1234",
+        name: "store 1",
+      },
+      status: "active",
+      speciality: <Select 
+                    title="speciality" 
+                    isDisabled={true}
+                    width="w-[130px]" 
+                    bgColor="bg-transparent" 
+                    borderWidth="border" 
+                    borderStyle="border-solid" 
+                    borderColor="border-black" 
+                    options={["speciality 1", "speciality 2", "speciality 3"]} 
+                />,
+      wilaya: "wilaya 1",
+    },
+    {
+      name: "hamza 1234",
+      email: "hamza1234@gmail.com",
+      phone: "0555555555",
+      startDate: "2021-09-20",
+      store: {
+        id: "1234",
+        name: "store 1",
+      },
+      status: "active",
+      speciality: <Select 
+                    title="speciality" 
+                    isDisabled={true}
+                    width="w-[130px]" 
+                    bgColor="bg-transparent" 
+                    borderWidth="border" 
+                    borderStyle="border-solid" 
+                    borderColor="border-black" 
+                    options={["speciality 1", "speciality 2", "speciality 3"]} 
+                />,
+      wilaya: "wilaya 1",
+    },
+    {
+      name: "hamza 1234",
+      email: "hamza1234@gmail.com",
+      phone: "0555555555",
+      startDate: "2021-09-20",
+      store: {
+        id: "1234",
+        name: "store 1",
+      },
+      status: "active",
+      speciality: <Select 
+                    title="speciality" 
+                    isDisabled={true}
+                    width="w-[130px]" 
+                    bgColor="bg-transparent" 
+                    borderWidth="border" 
+                    borderStyle="border-solid" 
+                    borderColor="border-black" 
+                    options={["speciality 1", "speciality 2", "speciality 3"]} 
+                />,
+      wilaya: "wilaya 1",
+    },
+    {
+      name: "hamza 1234",
+      email: "hamza1234@gmail.com",
+      phone: "0555555555",
+      startDate: "2021-09-20",
+      store: {
+        id: "1234",
+        name: "store 1",
+      },
+      status: "active",
+      speciality: <Select 
+                    title="speciality" 
+                    isDisabled={true}
+                    width="w-[130px]" 
+                    bgColor="bg-transparent" 
+                    borderWidth="border" 
+                    borderStyle="border-solid" 
+                    borderColor="border-black" 
+                    options={["speciality 1", "speciality 2", "speciality 3"]} 
+                />,
+      wilaya: "wilaya 1",
+    },
+  ];
