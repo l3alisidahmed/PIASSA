f9cf22edf61b6a7e5c45f2d36c4e05c43f8bc2f2
l3alisidahmed <infouhbc001@gmail.com>
2025-01-30 17:39:10 +0100

    add some porps and logiqye

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -6,9 +6,15 @@ import { MapPin, ChevronDown } from "lucide-react";
 interface SelectProps {
     title: string;
     options: String[];
+    isDisabled?: boolean;
+    bgColor?: string;
+    borderColor?: string;
+    borderWidth?: string;
+    borderStyle?: string;
+    width?: string;
 }
 
-const Select : React.FC<SelectProps> = ({title, options}) => {
+const Select : React.FC<SelectProps> = ({title, options, isDisabled, width, bgColor, borderWidth, borderStyle, borderColor}) => {
 
     const [isOpen, setIsOpen] = useState(false);
     const [selectedOption, setSelectedOption] = useState<String | null>(null);
@@ -21,14 +27,14 @@ const Select : React.FC<SelectProps> = ({title, options}) => {
     }
 
     return (
-        <div className="relative w-[200px]" ref={dropDownRef}>
+        <div className={`relative ${width || 'w-[200px]'}`} ref={dropDownRef}>
             <button 
-                className={`w-full flex flex-row justify-around items-center gap-2 ${isOpen ? 'bg-white/60' : 'bg-black/15'} p-2 ${isOpen ? 'rounded-t-md' : 'rounded-md'}`} 
+                className={`w-full flex flex-row justify-around items-center gap-2 ${isOpen ? 'bg-white/60' : isDisabled ? bgColor : 'bg-black/15'} p-2 ${isOpen ? 'rounded-t-md' : 'rounded-md'} ${borderWidth} ${borderStyle} ${borderColor}`} 
                 onClick={() => setIsOpen(!isOpen)}
                 aria-haspopup='listbox'
                 aria-expanded={isOpen}
             >
-                <MapPin size={16} />
+                {isDisabled ? null : <MapPin size={16} />}
                 <p>{selectedOption ? selectedOption : title}</p>
                 <ChevronDown size={16} className={`transition-transform ${isOpen ? 'transform rotate-180' : null}`} />
             </button>
