2bbd428e90d4e0828fcebb27c3db78b256db3b46
l3alisidahmed <infouhbc001@gmail.com>
2025-01-30 09:38:19 +0100

    complete the code of header and add some logique

--- src/components/ui/TopBar.tsx
+++ src/components/ui/TopBar.tsx
@@ -1,13 +1,42 @@
 import Search from "./Search";
+import Select from "./Select";
 
+const Options = [
+    "Adrar",
+    "Chlef",
+    "Laghouat",
+    "Oum El Bouaghi",
+    "Batna",
+    "Béjaïa",
+    "Biskra",
+    "Béchar",
+    "Blida",
+    "Bouira",
+    "Tamanrasset",
+    "Tébessa",
+    "Tlemcen",
+    "Tiaret",
+    "Tizi Ouzou",
+    "Alger",
+    "Djelfa",
+]
 
 const TopBar = () => {
     return (
-        <div className="w-screen h-20 bg-[#FF3D00] flex flex-row items-center justify-between p-5">
+        <div className="h-20 flex flex-row items-center justify-around px-5">
             <div className="flex flex-row items-center gap-5">
                 <Search />
+                <Select title="Select Wilaya" options={Options}/>
             </div>
-            <div></div>
+            <div className="flex flex-row items-center gap-5">
+                <div>
+                    <p>Admin Name</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                    <p>1/30/2025</p>
+                    <p>12:00 PM</p>
+                </div>
+            </div>
         </div>
     );
 }
