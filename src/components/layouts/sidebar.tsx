41c5075d13f2f34297c750003419db2d7c584c9a
l3alisidahmed <infouhbc001@gmail.com>
2025-01-30 09:35:53 +0100

    change the style of sidbare and make the code readable

--- src/components/layouts/sidebar.tsx
+++ src/components/layouts/sidebar.tsx
@@ -11,12 +11,21 @@ export default function Sidebar() {
     const [tvIconClicked, setTvIconClicked] = useState(false);
 
     return (
-        <div className="flex flex-col items-center gap-10 p-20">
+        <div className="bg-white/20 flex flex-col items-center gap-10 p-20">
             <Logo />
 
             <div className="backdrop-blur-md bg-white/10 flex flex-col items-center justify-center p-2 rounded-full">
-                <IconCard icon={<Handshake size={30} onClick={() => {setHandIconClicked(true); setTvIconClicked(false)}} />} bgColor={handIconClicked ? '#FF3D00' : undefined} rounded='rounded-t-full' />
-                <IconCard icon={<TvMinimal size={30} onClick={() => {setTvIconClicked(true); setHandIconClicked(false)}} />} bgColor={tvIconClicked ? '#FF3D00' : undefined} rounded='rounded-b-full' />
+                <IconCard 
+                    icon={
+                        <Handshake size={30} onClick={() => {setHandIconClicked(true); setTvIconClicked(false)}} />} 
+                    bgColor={handIconClicked ? '#FF3D00' : undefined} 
+                    rounded='rounded-t-full' 
+                />
+                <IconCard 
+                    icon={<TvMinimal size={30} onClick={() => {setTvIconClicked(true); setHandIconClicked(false)}} />} 
+                    bgColor={tvIconClicked ? '#FF3D00' : undefined} 
+                    rounded='rounded-b-full' 
+                />
             </div>
 
             {handIconClicked ? 
