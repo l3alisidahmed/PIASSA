f013e1756de12e99fb4d28bec2d31850e140e991
l3alisidahmed <infouhbc001@gmail.com>
2025-01-31 17:05:08 +0100

    split the content in a onther small components

--- src/components/ui/DataRow.tsx
+++ src/components/ui/DataRow.tsx
@@ -1,65 +1,27 @@
 'use client';
 
-import React, { useRef, useState } from "react";
-import { ChevronRight } from "lucide-react";
-import IconCard from "./IconCard";
+import { useForm } from "@/providers/FormProvider";
+import { useSearch } from "@/providers/SearchProvider";
+import PartnerCard from "./PartnerCard";
 
-interface DataRowProps {
-    data: any[];
-}
 
-const DataRow : React.FC<DataRowProps> = ({ data }) => {
+const DataRow = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
+    const { partnerData } = useForm();
 
-    const handleClicked = () => {
-        setIsOpen(!isOpen);
-    }
+    const { search } = useSearch();
 
     return (
-        <div className="flex flex-col gap-5">
-            {data.map((row, index) => (
-                <div key={index} className={`grid grid-cols-[1fr_100px] gap-5`}>
-                    <div className="w-full p-5 flex flex-row justify-between items-center bg-white text-black">
-                        <div className="flex flex-col gap-2 justify-start">
-                            <p>{row.name}</p>
-                            <p>{row.email}</p>
-                        </div>
-                        <div className="flex flex-col gap-2 justify-start">
-                            <p>Wilaya</p>
-                            <p>{row.wilaya}</p>
-                        </div>
-                        <div className="flex flex-col gap-2 justify-start">
-                            {Array.isArray(row.phone) ? row.phone.map((phone:any, index:any) => (
-                                <p key={index}>{phone}</p>
-                            )) : <p>{row.phone}</p>}
-                        </div>
-                        <div className="flex flex-col gap-2 justify-start">
-                            <p>{row.startDate}</p>
-                        </div>
-                        <div className="flex flex-col gap-2 justify-start">
-                            <p>{row.store.name}</p>
-                            <p>{row.store.id}</p>
-                        </div>
-                        <div className="text-center text-green-500">
-                            <p>{row.status}</p>
-                        </div>
-                        <div>
-                            {row.speciality}
-                        </div>
-                    </div>
-                    <div className="flex flex-row justify-center items-center w-20">
-                        <IconCard icon={<ChevronRight size={30} />} bgColor="rgba(255, 255, 255, 0.3)" onClick={() => handleClicked()} />
-                        {isOpen ? (
-                            <div className="flex flex-row gap-2">
-                                <IconCard icon={<ChevronRight size={30} />} bgColor="rgba(255, 255, 255, 0.3)" />
-                                <IconCard icon={<ChevronRight size={30} />} bgColor="rgba(255, 255, 255, 0.3)" />
-                                <IconCard icon={<ChevronRight size={30} />} bgColor="rgba(255, 255, 255, 0.3)" />
-                            </div>
-                        ) : null}
-                    </div>
-                </div>
-            ))}
+        <div className="flex flex-col gap-5 overflow-auto h-[630px] scrollbar-hide">
+            {search === "" ? (
+                partnerData.map((partner:any, index:any) => (
+                    <PartnerCard key={index} partner={partner} />
+                ))
+            ) : (
+                partnerData.filter((partner:any) => partner.name.toLowerCase().includes(search.toLowerCase())).map((partner:any, index:any) => (
+                    <PartnerCard key={index} partner={partner} />
+                ))
+            )}
         </div>
     );
 }
