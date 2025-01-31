62491bbff4b2c2b23d16751f1bb0d1d54edb3538
l3alisidahmed <infouhbc001@gmail.com>
2025-01-31 17:04:19 +0100

    make data clear

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -1,119 +1,67 @@
-import Select from "@/components/ui/Select";
-
 export const data = [
     {
       name: "hamza 1234",
       email: "hamza1234@gmail.com",
-      phone: "0555555555",
+      phone: ["0555555555"],
       startDate: "2021-09-20",
       store: {
         id: "1234",
         name: "store 1",
       },
       status: "active",
-      speciality: <Select 
-                    title="speciality" 
-                    isDisabled={true}
-                    width="w-[130px]"
-                    bgColor="bg-transparent" 
-                    borderWidth="border" 
-                    borderStyle="border-solid" 
-                    borderColor="border-black" 
-                    options={["speciality 1", "speciality 2", "speciality 3"]}
-                    zIndex={"z-0"}
-                />,
+      speciality: 'speciality 1',
       wilaya: "wilaya 1",
     },
     {
       name: "hamza 1234",
       email: "hamza1234@gmail.com",
-      phone: "0555555555",
+      phone: ["0555555555"],
       startDate: "2021-09-20",
       store: {
         id: "1234",
         name: "store 1",
       },
       status: "active",
-      speciality: <Select 
-                    title="speciality" 
-                    isDisabled={true}
-                    width="w-[130px]" 
-                    bgColor="bg-transparent" 
-                    borderWidth="border" 
-                    borderStyle="border-solid" 
-                    borderColor="border-black" 
-                    options={["speciality 1", "speciality 2", "speciality 3"]} 
-                    zIndex={"z-0"}
-                />,
+      speciality: 'speciality 1',
       wilaya: "wilaya 1",
     },
     {
       name: "hamza 1234",
       email: "hamza1234@gmail.com",
-      phone: "0555555555",
+      phone: ["0555555555"],
       startDate: "2021-09-20",
       store: {
         id: "1234",
         name: "store 1",
       },
       status: "active",
-      speciality: <Select 
-                    title="speciality" 
-                    isDisabled={true}
-                    width="w-[130px]" 
-                    bgColor="bg-transparent" 
-                    borderWidth="border" 
-                    borderStyle="border-solid" 
-                    borderColor="border-black" 
-                    options={["speciality 1", "speciality 2", "speciality 3"]} 
-                    zIndex={"z-0"}
-                />,
+      speciality: 'speciality 1',
       wilaya: "wilaya 1",
     },
     {
       name: "hamza 1234",
       email: "hamza1234@gmail.com",
-      phone: "0555555555",
+      phone: ["0555555555"],
       startDate: "2021-09-20",
       store: {
         id: "1234",
         name: "store 1",
       },
       status: "active",
-      speciality: <Select 
-                    title="speciality" 
-                    isDisabled={true}
-                    width="w-[130px]" 
-                    bgColor="bg-transparent" 
-                    borderWidth="border" 
-                    borderStyle="border-solid" 
-                    borderColor="border-black" 
-                    options={["speciality 1", "speciality 2", "speciality 3"]} 
-                    zIndex={"z-0"}
-                />,
+      speciality: 'speciality 1',
       wilaya: "wilaya 1",
     },
     {
       name: "hamza 1234",
       email: "hamza1234@gmail.com",
-      phone: "0555555555",
+      phone: ["0555555555"],
       startDate: "2021-09-20",
       store: {
         id: "1234",
         name: "store 1",
       },
       status: "active",
-      speciality: <Select 
-                    title="speciality" 
-                    isDisabled={true}
-                    width="w-[130px]" 
-                    bgColor="bg-transparent" 
-                    borderWidth="border" 
-                    borderStyle="border-solid" 
-                    borderColor="border-black" 
-                    options={["speciality 1", "speciality 2", "speciality 3"]} 
-                    zIndex={"z-0"}
-                />,
+      speciality: 'speciality 1',
       wilaya: "wilaya 1",
     },
   ];
