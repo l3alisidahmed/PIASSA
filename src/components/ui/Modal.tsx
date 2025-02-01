3fa2af10bc175307f0221f3fcd5fafe9270b1849
l3alisidahmed <infouhbc001@gmail.com>
2025-02-01 13:04:46 +0100

    change the code

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,18 +1,17 @@
 import Form from "./Form";
 
 interface ModalProps {
+    children?: React.ReactNode;
     showModal: boolean;
     onClose: () => void;
 }
 
-const Modal : React.FC<ModalProps> = ({showModal, onClose}) => {
+const Modal : React.FC<ModalProps> = ({children, showModal, onClose}) => {
     if (!showModal) return null;
     
     return (
         <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex justify-center items-center z-50">
-            <div className="bg-white/80 shadow-lg backdrop-blur-[60px] p-10 rounded-lg">
-                <Form onClose={onClose} />
-            </div>
+            {children}
         </div>
     );
 }
