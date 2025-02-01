44b0f532e54d2132e8bd4de11ca724daa7988562
l3alisidahmed <infouhbc001@gmail.com>
2025-02-01 21:10:55 +0100

    add animation

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,5 +1,4 @@
-import Form from "./Form";
-
+import * as motion from "motion/react-client"
 interface ModalProps {
     children?: React.ReactNode;
     showModal: boolean;
@@ -10,9 +9,9 @@ const Modal : React.FC<ModalProps> = ({children, showModal, onClose}) => {
     if (!showModal) return null;
     
     return (
-        <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex justify-center items-center z-50">
+        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.2}} className="fixed inset-0 bg-black/30 backdrop-blur-sm flex justify-center items-center z-50">
             {children}
-        </div>
+        </motion.div>
     );
 }
 
