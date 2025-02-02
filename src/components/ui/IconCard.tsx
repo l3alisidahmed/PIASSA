5abc75656388aceb6529031b3b8ac0ab8cb3cc16
l3alisidahmed <infouhbc001@gmail.com>
2025-02-02 11:26:34 +0100

    the last changes

--- src/components/ui/IconCard.tsx
+++ src/components/ui/IconCard.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
 
 interface IconCardProps {
     icon?: React.ReactNode;
@@ -9,14 +10,38 @@ interface IconCardProps {
 }
 
 const IconCard : FC<IconCardProps> = ({icon, bgColor, rounded, opacity, onClick}) => {
+
+    const iconButtonRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        iconButtonRef.current?.addEventListener('click', () => {
+            const className = iconButtonRef.current?.attributes[0].value.split(' ')[0];
+            if (className) {
+                document.querySelectorAll(`.${className}`).forEach((element) => {
+                    const TogleTopIconButton = element.attributes[0].value.includes('rounded-t-full') && element;
+                    const TogleBottomIconButton = element.attributes[0].value.includes('rounded-b-full') && element;
+                    if (element !== iconButtonRef.current && element !== TogleTopIconButton && element !== TogleBottomIconButton) {
+                        element.style.backgroundColor = 'transparent';
+                    } else {
+                        if (iconButtonRef.current) {
+                            iconButtonRef.current.style.backgroundColor = '#FF3D00';
+                        }
+                    }
+                });
+            }
+        });
+    }, []);
+
     return (
-        <div 
-            className={`w-50 h-50 backdrop-blur-md bg-white/5 flex flex-col items-center justify-center p-2 ${rounded || 'rounded-lg'} ${opacity || 'opacity-100'} cursor-pointer`} 
+        <motion.div
+            whileHover={{ scale: 1.1 }}
+            className={`icon-button w-50 h-50 backdrop-blur-md bg-white/5 flex flex-col items-center justify-center p-2 ${rounded || 'rounded-lg'} ${opacity || 'opacity-100'} cursor-pointer`} 
             style={{ backgroundColor: bgColor || 'transparent' }}
             onClick={onClick}
+            ref={iconButtonRef}
         >
             {icon? icon : null}
-        </div>
+        </motion.div>
     );
 }
 
--- src/components/ui/PartnerCard.tsx
+++ src/components/ui/PartnerCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ChevronRight, Pencil, Eye, Trash2, ChevronLeft} from "lucide-react";
 import IconCard from "./IconCard";
 import Select from "./Select";
@@ -10,16 +10,41 @@ import Delete from "./Delete";
 import * as motion from "motion/react-client";
 import ExitAnimation from "./ExitAnimation";
 import { PartnerType } from "@/providers/FormProvider";
+import { log } from "node:console";
 
 
 const PartnerCard = ({ partner, partnerKey }: { partnerKey?:string | number, partner: PartnerType }) => {
-    
+
     const [isOpen, setIsOpen] = useState(false);
     const [edit, setEdit] = useState(false);
     const [view, setView] = useState(false);
     const [remove, setRemove] = useState(false);
-
+    const [editData, setEditData] = useState<{ [key: string]: string }>({})
+    
     const dropDownRef = useRef<HTMLDivElement>(null);
+    
+    useEffect(() => {
+        console.log(dropDownRef.current);
+        console.log(dropDownRef.current?.parentElement);
+        console.log(dropDownRef.current?.parentElement?.childNodes);
+        console.log(dropDownRef.current?.parentElement?.childNodes[0]);
+        console.log(dropDownRef.current?.parentElement?.childNodes[0].childNodes);
+        if (dropDownRef.current?.parentElement) {
+            const data: { [key: string]: string } = {};
+            dropDownRef.current?.parentElement?.childNodes[0].childNodes.forEach((element, index) => {
+                element.childNodes.forEach((elementChild) => {
+                    const childElement = elementChild as HTMLElement;
+                    if (childElement.textContent) {
+                        data[`${childElement.id}`] = childElement.textContent;
+                    }
+                });
+            });
+            setEditData(data);
+        }
+
+        console.log(editData);
+
+    }, [isOpen])
 
     const handleClicked = () => {
         setIsOpen(!isOpen);
@@ -30,35 +55,35 @@ const PartnerCard = ({ partner, partnerKey }: { partnerKey?:string | number, par
             <div key={partnerKey} className={`relative grid ${isOpen ? 'grid-cols-[1fr_250px] right-20' : 'grid-cols-[1fr_100px]'} gap-5`}>
                 <div className="w-full p-5 flex flex-partner justify-between items-center bg-white text-black">
                     <div className="flex flex-col gap-2 justify-start">
-                        <p>{partner.name}</p>
-                        <p>{partner.email}</p>
+                        <p id="name">{partner.name}</p>
+                        <p id="email">{partner.email}</p>
                     </div>
                     <div className="flex flex-col gap-2 justify-start">
                         <p>Wilaya</p>
-                        <p>{partner.wilaya}</p>
+                        <p id="wilaya">{partner.wilaya}</p>
                     </div>
                     <div className="flex flex-col gap-2 justify-start">
                         {partner.phone.map((phone:string, index:number) => (
-                            <p key={index}>{phone}</p>
+                            <p key={index} id={`phone_${index + 1}`}>{phone}</p>
                         ))}
                     </div>
                     <div className="flex flex-col gap-2 justify-start">
-                        <p>{partner.startDate}</p>
+                        <p id="startDate">{partner.startDate}</p>
                     </div>
                     <div className="flex flex-col gap-2 justify-start">
-                        <p>{partner.store.name}</p>
-                        <p>{partner.store.id}</p>
+                        <p id="storeName">{partner.store.name}</p>
+                        <p id="storeID">{partner.store.id}</p>
                     </div>
                     <div className="text-center text-green-500">
-                        <p>{partner.status}</p>
+                        <p id="status">{partner.status}</p>
                     </div>
                     <div>
-                        <Select key="data-select-key" title={partner.speciality} isDisabled={true} width="w-[130px]" bgColor="bg-transparent" borderWidth="border" borderStyle="border-solid" borderColor="border-black" options={["speciality 1", "speciality 2", "speciality 3"]} zIndex={"z-0"}/>,
+                        <Select key="data-select-key" title={partner.speciality} isDisabled={true} width="w-[130px]" bgColor="bg-transparent" borderWidth="border" borderStyle="border-solid" borderColor="border-black" options={["speciality 1", "speciality 2", "speciality 3"]} zIndex={"z-1"}/>
                     </div>
                 </div>
                 <div className="relative w-full flex flex-row items-center" ref={dropDownRef}>
                     <button onClick={() => handleClicked()} aria-haspopup='listbox' aria-expanded={isOpen}>
-                        <IconCard icon={isOpen ? <ChevronLeft size={30}/> : <ChevronRight size={30} />} bgColor="rgba(255, 255, 255, 0.3)" />
+                        <IconCard icon={<ChevronRight size={30} className={`transition-transform ${isOpen ? 'transform rotate-180' : null}`} />} bgColor="rgba(255, 255, 255, 0.3)" />
                     </button>
                     {isOpen ? (
                         <motion.div initial={{x: -150 }} animate={{x:0}} transition={{stiffness: 120}} exit={{x:100}} className="absolute right-10 flex flex-row gap-2">
@@ -69,17 +94,17 @@ const PartnerCard = ({ partner, partnerKey }: { partnerKey?:string | number, par
                     ) : null}
                 </div>
             </div>
-            {view && 
+            {(view && isOpen) && 
                 <ExitAnimation>
                     <View />
                 </ExitAnimation>
             }
-            {edit &&
+            {(edit && isOpen) &&
                 <ExitAnimation> 
-                    <Edit onClose={() => setEdit(false)} />
+                    <Edit onClose={() => setEdit(false)}  value={editData}/>
                 </ExitAnimation>
             }
-            {remove &&
+            {(remove && isOpen) &&
                 <Delete remove={remove} onClose={() => setRemove(false)} />
             }
         </>
